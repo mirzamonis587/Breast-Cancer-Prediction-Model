085928907c29270de0f5bf3cd0bf8788f2bc1181
mirzamonis587 <monisali587@gmail.com>
2026-03-10 13:42:33 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+import pandas as pd
+import numpy as np
+import pickle
+
+# load model
+model = pickle.load(open("model.pkl",'rb'))
+
+
+# flask app
+app=Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=['POST'])
+def predict ():
+    features= request.form['features']
+    features_list = features.split(',')
+    np_features = np.asarray(features_list, dtype = np.float32)
+    pred = model.predict(np_features.reshape(1,-1))
+
+    output = ["cancerous" if pred[0] ==1 else "not cancerous"]
+
+    return render_template('index.html',message = output)
+
+
+# python main
+if __name__ =="__main__":
+    app.run(debug=True)
+
